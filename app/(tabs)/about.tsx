49affde2dfd57879b0efaabe4402c35524c9ff88
Nagy Ábel <abel.nagy2601@gmail.com>
2024-08-08 22:27:42 +0200

    fix: about page font sizes

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -18,15 +18,15 @@ export default function Index () {
                 gap: 10
             }}
         >
-            <Text style={{fontSize: "30em", fontWeight: "bold"}}>About</Text>
+            <Text style={{fontSize: 30, fontWeight: "bold"}}>About</Text>
             <View style={{borderStartWidth: 3, borderStartColor: "black",}}>
-                <Text style={{fontSize: "20em", fontWeight: "bold", padding: 10}}>This app was made by 2 highschool students, who had enough of remembering debts.</Text>
+                <Text style={{fontSize: 20, fontWeight: "bold", padding: 10}}>This app was made by 2 highschool students, who had enough of remembering debts.</Text>
             </View>
-            <Text style={{fontSize: "20em", fontWeight: "bold", backgroundColor: "black", padding: 20, width:"100%", borderRadius: 15, color: "white", overflow: 'hidden'}}>Debtzap allows you to track your debts and write down others'.</Text>
-            <Text style={{fontSize: "20em", fontWeight: "bold"}}>You can find us on GitHub.</Text>
-            <A style={{fontSize: "20em", backgroundColor: "gray", color: "white", padding: 5, borderRadius: 5, overflow: "hidden"}} href="https://github.com/barnagoz"><AntDesign name="github" size={24} color="white" /> Barnabás Gőz</A>
-            <A style={{fontSize: "20em", backgroundColor: "gray", color: "white", padding: 5, borderRadius: 5, overflow: "hidden"}} href="https://github.com/abelanagy"><AntDesign name="github" size={24} color="white" /> Ábel Nagy</A>
-            <Text style={{position: "absolute", bottom: 20, fontSize: "20em", alignSelf: "center"}}>Thanks for using our application!</Text>
+            <Text style={{fontSize: 20, fontWeight: "bold", backgroundColor: "black", padding: 20, width:"100%", borderRadius: 15, color: "white", overflow: 'hidden'}}>Debtzap allows you to track your debts and write down others'.</Text>
+            <Text style={{fontSize: 20, fontWeight: "bold"}}>You can find us on GitHub.</Text>
+            <A style={{fontSize: 20, backgroundColor: "gray", color: "white", padding: 5, borderRadius: 5, overflow: "hidden"}} href="https://github.com/barnagoz"><AntDesign name="github" size={24} color="white" /> Barnabás Gőz</A>
+            <A style={{fontSize: 20, backgroundColor: "gray", color: "white", padding: 5, borderRadius: 5, overflow: "hidden"}} href="https://github.com/abelanagy"><AntDesign name="github" size={24} color="white" /> Ábel Nagy</A>
+            <Text style={{position: "absolute", bottom: 20, fontSize: 20, alignSelf: "center"}}>Thanks for using our application!</Text>
         </SafeAreaView>
     );
 }
