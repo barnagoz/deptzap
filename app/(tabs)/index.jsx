28c869b765d639c9bdc8d1f7fd4be1d93bde2898
Barnabás Gőz <barnagoz@icloud.com>
2024-08-11 16:59:30 +0200

    feat: logic to add depts

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,65 +1,92 @@
-import {Text, View, StyleSheet, Pressable, Share} from "react-native";
+import {Pressable, ScrollView, Share, StyleSheet, Text, View} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import Entypo from '@expo/vector-icons/Entypo';
-import { Link } from 'expo-router';
+import {Link} from 'expo-router';
+import {getDeptList} from "../../lib/depts";
+import {useEffect, useState} from "react";
 
 
 export default function Index () {
+    const [depts, setDepts] = useState([
+    ]);
+
+    async function getData () {
+        const depts = await getDeptList();
+        console.log(depts);
+        setDepts(depts);
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
     return (
-        <SafeAreaView
-            style={{
-                flex: 1,
+        <SafeAreaView style={{heigth: "100%"}}
+        >
+            <ScrollView style={{height: "105%"}} contentContainerStyle={{
                 justifyContent: "start",
                 alignItems: "start",
                 padding: 15,
-                gap: 10
-            }}
-        >
-        <Text style={{ fontSize: "30em", fontWeight: "bold" }}>Hello World!</Text>
-        <View style={{ backgroundColor: "#1C1C1C", padding: 20, width: "100%", borderRadius: 15 }}>
-            <Text style={{ color: "white" }}>Debt</Text>
-            <Text style={{ fontSize: "35em", color: "white" }}>$2.00</Text>
-        </View>
-        <Link href="/newtransaction" asChild>
-            <Pressable style={styles.button}>
-                <Text style={styles.text}>New transaction</Text>
-            </Pressable>
-        </Link>
-        <Text style={{ fontSize: "25em", marginTop: 10, fontWeight: "600" }}>Recent transactions</Text>
-        <Pressable onPress={() => Share.share({ message: "Hey bro, you owe me 100 dollars." })}>
-            <View style={{ backgroundColor: "#ff686b", padding: 20, width: "100%", borderRadius: 15, flex: 1, flexDirection: "row", maxHeight: 90, justifyContent: "space-between", alignItems: "center" }}>
-                <Entypo name="circle-with-minus" size={50} color="black" />
-                <Text style={{ fontSize: "20em" }}>János Béla</Text>
-                <Text style={{ fontSize: "35em", color: "black" }}>$2.00</Text>
+                gap: 10,
+                flexGrow: 1,
+                paddingBottom: 150
+            }}>
+            <Text style={{fontSize: "30em", fontWeight: "bold"}}>Hello World!</Text>
+            <View style={{backgroundColor: "#1C1C1C", padding: 20, width: "100%", borderRadius: 15}}>
+                {/* "TODO: remove this, as it can't be calculated due to different currencies */}
+                <Text style={{color: "white"}}>Debt</Text>
+                <Text style={{fontSize: "35em", color: "white"}}>$2.00</Text>
             </View>
-        </Pressable>
-        <Pressable onPress={() => Share.share({ message: "Hey bro, you owe me 100 dollars." })}>
-                <View style={{ backgroundColor: "#84dcc6", padding: 20, width: "100%", borderRadius: 15, flex: 1, flexDirection: "row", maxHeight: 90, justifyContent: "space-between", alignItems: "center" }}>
-                    <Entypo name="circle-with-plus" size={50} color="black" />
-                    <Text style={{ fontSize: "20em" }}>János Béla</Text>
-                    <Text style={{ fontSize: "35em", color: "black" }}>$4.00</Text>
-                </View>
-        </Pressable>
+            <Link href="/newtransaction" asChild>
+                <Pressable style={styles.button}>
+                    <Text style={styles.text}>New transaction</Text>
+                </Pressable>
+            </Link>
+            <Text style={{fontSize: "25em", marginTop: 10, fontWeight: "600"}}>Recent transactions</Text>
+                {/* TODO: add a "no transactions" message if there are no transactions */}
+                {/* TODO: add option to remove transactions */}
+                {/* TODO: change the share message to include the description and date of the transaction */}
+            {depts.map((dept, index) => (
+                <Pressable onPress={() => Share.share({message: "Hey bro, you owe me 100 dollars."})}>
+                    <View style={{
+                        backgroundColor: dept.amount > 0 ? "#ff686b" : "#84dcc6",
+                        padding: 20,
+                        width: "100%",
+                        borderRadius: 15,
+                        flex: 1,
+                        flexDirection: "row",
+                        maxHeight: 90,
+                        justifyContent: "space-between",
+                        alignItems: "center"
+                    }}>
+                        <Entypo name={dept.amount > 0 ? "circle-with-minus" : "circle-with-plus"} size={40}
+                                color="black"/>
+                        <Text style={{fontSize: "20em"}}>{dept.name}</Text>
+                        <Text style={{fontSize: "30em", color: "black"}}>{Math.abs(dept.amount)} {dept.currency}</Text>
+                    </View>
+                </Pressable>
+            ))}
+            </ScrollView>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 15,
-      elevation: 3,
-      width: "100%",
-      backgroundColor: '#1C1C1C',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 15,
+        elevation: 3,
+        width: "100%",
+        backgroundColor: '#1C1C1C',
     },
     text: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
     },
-  });
+});
--- app/(tabs)/newtransaction.tsx
+++ app/(tabs)/newtransaction.tsx
@@ -1,11 +1,20 @@
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Text, View, Switch, TextInput, Pressable, Keyboard} from "react-native";
-import { useState } from 'react';
+import {Pressable, Switch, Text, TextInput, View} from "react-native";
+import {useState} from 'react';
+import {addDept} from "@/lib/depts";
 
-export default function Adddebt () {
+export default function Adddebt() {
     const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
     const [amount, onChangeAmount] = useState('');
+    const [name, onChangeName] = useState('');
+    const [currency, onChangeCurrency] = useState('');
+    const [desc, onChangeDesc] = useState('');
+
+    async function submit() {
+        await addDept(name, isEnabled ? amount : -amount, currency, desc, new Date().toISOString());
+        alert("Transaction saved!")
+    }
+
     return (
         <SafeAreaView style={{
             flex: 1,
@@ -14,43 +23,153 @@ export default function Adddebt () {
             padding: 15,
             gap: 10,
         }}>
-        <Text style={{fontSize: 30, fontWeight: "bold", alignSelf: "center"}}>New transaction</Text>
-        <View style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', flexDirection: "row", maxHeight: 30, gap: 5, alignSelf: "center", marginTop: 50}}>
-            <Text style={{fontSize: 20, alignSelf: "center", paddingRight: 15, fontWeight: "bold"}}>Add debt</Text>
-            <Switch
-                trackColor={{false: '#84dcc6', true: '#ff686b'}}
-                thumbColor={"#f4f3f4"}
-                ios_backgroundColor="#84dcc6"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-            />
-            <Text style={{fontSize: 20, alignSelf: "center", paddingLeft: 25, fontWeight: "bold"}}>I owe</Text>
-        </View>
-        <View style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', flexDirection: "row", gap: 5, alignSelf: "center", marginTop: 50, maxHeight: 100}}>
-        <TextInput
-            style={{ height: 50, borderColor: 'gray', borderWidth: 3, width: 300, borderRadius: 10, padding: 10, fontSize: 20}}
-            onChangeText={onChangeAmount}
-            value={amount}
-            placeholder="Amount"
-            keyboardType="numeric"
-            placeholderTextColor="gray"
-        />
-        <Text style={{fontSize: 40, paddingLeft: 10}}>$</Text>
-        </View>
-        <View style={{flex: 1, alignItems: "flex-start", justifyContent: 'flex-start', flexDirection: "row", gap: 5, alignSelf: "center", maxHeight: 50}}>
-            <Pressable
-                onPress={() => Keyboard.dismiss()}
-                accessibilityLabel="Back"
-                style={{backgroundColor: "#1c1c1c", paddingHorizontal: 20, borderRadius: 10, padding: 10}}>
-                <Text style={{fontSize: 20, color: "white"}}>Back</Text>
-            </Pressable>
-            <Pressable
-                onPress={() => Keyboard.dismiss()} // TODO: Add submit function
-                accessibilityLabel="Submit"
-                style={{backgroundColor: "#1c1c1c", padding: 10, borderRadius: 10, paddingHorizontal: 15}}>
-                <Text style={{fontSize: 20, color: "white"}}>Submit</Text>
-            </Pressable>
-        </View>
-    </SafeAreaView>
+            <Text style={{fontSize: 30, fontWeight: "bold", alignSelf: "flex-start"}}>New transaction</Text>
+            <Text style={{fontSize: 22.5, alignSelf: "flex-start"}}>Who owes who?</Text>
+            <View style={{
+                flex: 1,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: "row",
+                maxHeight: 30,
+                gap: 5,
+                alignSelf: "center",
+                marginTop: 15,
+                marginBottom: 15,
+            }}>
+                <Text style={{fontSize: 18, alignSelf: "center", paddingRight: 5, fontWeight: "semibold"}}>Someone owes
+                    me</Text>
+                <Switch
+                    trackColor={{false: '#84dcc6', true: '#ff686b'}}
+                    thumbColor={"#f4f3f4"}
+                    ios_backgroundColor="#84dcc6"
+                    onValueChange={() => setIsEnabled(previousState => !previousState)}
+                    value={isEnabled}
+                />
+                <Text style={{fontSize: 18, alignSelf: "center", paddingLeft: 5, fontWeight: "semibold"}}>I owe
+                    someone</Text>
+            </View>
+            <Text style={{
+                fontSize: 22.5,
+                alignSelf: "flex-start"
+            }}>{isEnabled ? "Who do you owe?" : "Who owes you?"}</Text>
+            <View style={{
+                flex: 1,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: "row",
+                gap: 5,
+                alignSelf: "center",
+                maxHeight: 50,
+                marginBottom: 15
+            }}>
+                <TextInput
+                    style={{
+                        height: 'auto',
+                        borderColor: 'gray',
+                        borderWidth: 2,
+                        width: "100%",
+                        borderRadius: 18.5,
+                        padding: 12,
+                        fontSize: 18,
+                    }}
+                    onChangeText={onChangeName}
+                    value={name}
+                    placeholder="Name"
+                    placeholderTextColor="gray"
+                />
+            </View>
+            <Text style={{
+                fontSize: 22.5,
+                alignSelf: "flex-start"
+            }}>{isEnabled ? "How much do you owe?" : "How much do they owe you?"}</Text>
+            <View style={{
+                flex: 1,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: "row",
+                alignSelf: "center",
+                maxHeight: 50,
+                marginBottom: 15
+            }}>
+                <TextInput
+                    style={{
+                        borderColor: 'gray',
+                        borderWidth: 2,
+                        borderRightWidth: 0,
+                        width: "70%",
+                        borderTopLeftRadius: 18.5,
+                        borderBottomLeftRadius: 18.5,
+                        padding: 12,
+                        fontSize: 18
+                    }}
+                    onChangeText={onChangeAmount}
+                    value={amount}
+                    placeholder="Amount"
+                    keyboardType="numeric"
+                    placeholderTextColor="gray"
+                />
+                <TextInput
+                    style={{
+                        borderColor: 'gray',
+                        borderWidth: 2,
+                        width: "30%",
+                        borderTopRightRadius: 18.5,
+                        borderBottomRightRadius: 18.5,
+                        padding: 12,
+                        fontSize: 18
+                    }}
+                    onChangeText={onChangeCurrency}
+                    value={currency}
+                    placeholder="Currency"
+                    placeholderTextColor="gray"
+                />
+            </View>
+            <Text style={{
+                fontSize: 22.5,
+                alignSelf: "flex-start"
+            }}>{isEnabled ? "Why do you owe them?" : "Why do they owe you?"}</Text>
+            <View style={{
+                flex: 1,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: "row",
+                gap: 5,
+                alignSelf: "center",
+                maxHeight: 50,
+                marginBottom: 15
+            }}>
+                <TextInput
+                    style={{
+                        height: 'auto',
+                        borderColor: 'gray',
+                        borderWidth: 2,
+                        width: "100%",
+                        borderRadius: 18.5,
+                        padding: 12,
+                        fontSize: 18,
+                    }}
+                    onChangeText={onChangeDesc}
+                    value={desc}
+                    placeholder="Short description"
+                    placeholderTextColor="gray"
+                />
+            </View>
+            <View style={{
+                flex: 1,
+                alignItems: "flex-start",
+                justifyContent: 'flex-start',
+                flexDirection: "row",
+                gap: 5,
+                alignSelf: "center",
+                maxHeight: 50
+            }}>
+                <Pressable
+                    onPress={() => submit()}
+                    accessibilityLabel="Submit"
+                    style={{backgroundColor: "#1c1c1c", padding: 10, borderRadius: 18.5, paddingHorizontal: 15, width: "100%", flex: 1, alignItems: "center"}}>
+                    <Text style={{fontSize: 20, color: "white"}}>Save</Text>
+                </Pressable>
+            </View>
+        </SafeAreaView>
     );
 }
