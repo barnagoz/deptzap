6ceda5040a2ef91ed9aa9e06e1cf9a4f38342d4f
Nagy Ábel <abel.nagy2601@gmail.com>
2025-01-06 23:31:29 +0100

    final changes before apple store submit wohoo

--- app/(tabs)/newtransaction.tsx
+++ app/(tabs)/newtransaction.tsx
@@ -1,188 +1,249 @@
-import {SafeAreaView} from "react-native-safe-area-context";
-import {Pressable, Switch, Text, TextInput, View} from "react-native";
-import {useState} from 'react';
-import {addDept} from "@/lib/depts";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Pressable, Switch, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { addDept } from "@/lib/depts";
 import { router } from "expo-router";
 
 export default function Adddebt() {
-    // States for the form
-    const [isEnabled, setIsEnabled] = useState(false);
-    const [amount, onChangeAmount] = useState('');
-    const [name, onChangeName] = useState('');
-    const [currency, onChangeCurrency] = useState('');
-    const [desc, onChangeDesc] = useState('');
+  // States for the form
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [amount, onChangeAmount] = useState("");
+  const [name, onChangeName] = useState("");
+  const [currency, onChangeCurrency] = useState("");
+  const [desc, onChangeDesc] = useState("");
 
-    // Function to submit the form
-    async function submit() {
-        await addDept(name, isEnabled ? amount : -amount, currency, desc, new Date().toISOString());
-        onChangeAmount('');
-        onChangeName('');
-        // onChangeCurrency(''); - removed because a user might want to use the same currency again
-        onChangeDesc('');
-        alert("Transaction saved!")
-        router.replace('/');
-
-    }
-
-    return (
-        <SafeAreaView style={{
-            flex: 1,
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-            padding: 15,
-            gap: 10,
-        }}>
-            <Text style={{fontSize: 30, fontWeight: "bold", alignSelf: "flex-start"}}>New transaction</Text>
-            <Text style={{fontSize: 22.5, alignSelf: "flex-start"}}>Who owes who?</Text>
-            <View style={{
-                flex: 1,
-                alignItems: 'flex-start',
-                justifyContent: 'flex-start',
-                flexDirection: "row",
-                maxHeight: 30,
-                gap: 5,
-                alignSelf: "center",
-                marginTop: 15,
-                marginBottom: 15,
-            }}>
-                <Text style={{fontSize: 18, alignSelf: "center", paddingRight: 5, fontWeight: "semibold"}}>Someone owes
-                    me</Text>
-                <Switch
-                    trackColor={{false: '#84dcc6', true: '#ff686b'}}
-                    thumbColor={"#f4f3f4"}
-                    ios_backgroundColor="#84dcc6"
-                    onValueChange={() => setIsEnabled(previousState => !previousState)}
-                    value={isEnabled}
-                />
-                <Text style={{fontSize: 18, alignSelf: "center", paddingLeft: 5, fontWeight: "semibold"}}>I owe
-                    someone</Text>
-            </View>
-            <Text style={{
-                fontSize: 22.5,
-                alignSelf: "flex-start"
-            }}>{isEnabled ? "Who do you owe?" : "Who owes you?"}</Text>
-            <View style={{
-                flex: 1,
-                alignItems: 'flex-start',
-                justifyContent: 'flex-start',
-                flexDirection: "row",
-                gap: 5,
-                alignSelf: "center",
-                maxHeight: 50,
-                marginBottom: 15
-            }}>
-                <TextInput
-                    style={{
-                        height: 'auto',
-                        borderColor: 'gray',
-                        borderWidth: 2,
-                        width: "100%",
-                        borderRadius: 18.5,
-                        padding: 12,
-                        fontSize: 18,
-                    }}
-                    onChangeText={onChangeName}
-                    value={name}
-                    placeholder="Name"
-                    placeholderTextColor="gray"
-                />
-            </View>
-            {/* Cool switch, which changes color based on state */}
-            <Text style={{
-                fontSize: 22.5,
-                alignSelf: "flex-start"
-            }}>{isEnabled ? "How much do you owe?" : "How much do they owe you?"}</Text>
-            <View style={{
-                flex: 1,
-                alignItems: 'flex-start',
-                justifyContent: 'flex-start',
-                flexDirection: "row",
-                alignSelf: "center",
-                maxHeight: 50,
-                marginBottom: 15
-            }}>
-                {/* Text inputs for the new transaction */}
-                <TextInput
-                    style={{
-                        borderColor: 'gray',
-                        borderWidth: 2,
-                        borderRightWidth: 0,
-                        width: "70%",
-                        borderTopLeftRadius: 18.5,
-                        borderBottomLeftRadius: 18.5,
-                        padding: 12,
-                        fontSize: 18
-                    }}
-                    onChangeText={onChangeAmount}
-                    value={amount}
-                    placeholder="Amount"
-                    keyboardType="numeric"
-                    placeholderTextColor="gray"
-                />
-                <TextInput
-                    style={{
-                        borderColor: 'gray',
-                        borderWidth: 2,
-                        width: "30%",
-                        borderTopRightRadius: 18.5,
-                        borderBottomRightRadius: 18.5,
-                        padding: 12,
-                        fontSize: 18
-                    }}
-                    onChangeText={onChangeCurrency}
-                    value={currency}
-                    placeholder="Currency"
-                    placeholderTextColor="gray"
-                />
-            </View>
-            <Text style={{
-                fontSize: 22.5,
-                alignSelf: "flex-start"
-            }}>{isEnabled ? "Why do you owe them?" : "Why do they owe you?"}</Text>
-            <View style={{
-                flex: 1,
-                alignItems: 'flex-start',
-                justifyContent: 'flex-start',
-                flexDirection: "row",
-                gap: 5,
-                alignSelf: "center",
-                height: 50,
-                marginBottom: 15,
-                maxHeight: 100
-            }}>
-                <TextInput
-                    style={{
-                        height: 'auto',
-                        borderColor: 'gray',
-                        borderWidth: 2,
-                        width: "100%",
-                        borderRadius: 18.5,
-                        padding: 12,
-                        fontSize: 18,
-                    }}
-                    onChangeText={onChangeDesc}
-                    value={desc}
-                    placeholder="Short description"
-                    placeholderTextColor="gray"
-                />
-            </View>
-            <View style={{
-                flex: 1,
-                alignItems: "flex-start",
-                justifyContent: 'flex-start',
-                flexDirection: "row",
-                gap: 5,
-                alignSelf: "center",
-                maxHeight: 50
-            }}>
-                {/* Submit button, saves the new transaction to on-device storage */}
-                <Pressable
-                    onPress={() => submit()}
-                    accessibilityLabel="Submit"
-                    style={{backgroundColor: "#1c1c1c", padding: 10, borderRadius: 18.5, paddingHorizontal: 15, width: "100%", flex: 1, alignItems: "center"}}>
-                    <Text style={{fontSize: 20, color: "white"}}>Save</Text>
-                </Pressable>
-            </View>
-        </SafeAreaView>
+  // Function to submit the form
+  async function submit() {
+    await addDept(
+      name,
+      isEnabled ? amount : -amount,
+      currency,
+      desc,
+      new Date().toISOString()
     );
+    onChangeAmount("");
+    onChangeName("");
+    // onChangeCurrency(''); - removed because a user might want to use the same currency again
+    onChangeDesc("");
+    alert("Transaction saved!");
+    router.replace("/");
+  }
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        padding: 15,
+        gap: 10,
+      }}
+    >
+      <Text
+        style={{ fontSize: 30, fontWeight: "bold", alignSelf: "flex-start" }}
+      >
+        New transaction
+      </Text>
+      <Text style={{ fontSize: 22.5, alignSelf: "flex-start" }}>
+        Who owes who?
+      </Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "row",
+          maxHeight: 30,
+          gap: 5,
+          alignSelf: "center",
+          marginTop: 15,
+          marginBottom: 15,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            alignSelf: "center",
+            paddingRight: 5,
+            fontWeight: "semibold",
+          }}
+        >
+          Someone owes me
+        </Text>
+        <Switch
+          trackColor={{ false: "#84dcc6", true: "#ff686b" }}
+          thumbColor={"#f4f3f4"}
+          ios_backgroundColor="#84dcc6"
+          onValueChange={() => setIsEnabled((previousState) => !previousState)}
+          value={isEnabled}
+        />
+        <Text
+          style={{
+            fontSize: 18,
+            alignSelf: "center",
+            paddingLeft: 5,
+            fontWeight: "semibold",
+          }}
+        >
+          I owe someone
+        </Text>
+      </View>
+      <Text
+        style={{
+          fontSize: 22.5,
+          alignSelf: "flex-start",
+        }}
+      >
+        {isEnabled ? "Who do you owe?" : "Who owes you?"}
+      </Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "row",
+          gap: 5,
+          alignSelf: "center",
+          maxHeight: 50,
+          marginBottom: 15,
+        }}
+      >
+        <TextInput
+          style={{
+            height: "auto",
+            borderColor: "gray",
+            borderWidth: 2,
+            width: "100%",
+            borderRadius: 18.5,
+            padding: 12,
+            fontSize: 18,
+          }}
+          onChangeText={onChangeName}
+          value={name}
+          placeholder="Name"
+          placeholderTextColor="gray"
+        />
+      </View>
+      {/* Cool switch, which changes color based on state */}
+      <Text
+        style={{
+          fontSize: 22.5,
+          alignSelf: "flex-start",
+        }}
+      >
+        {isEnabled ? "How much do you owe?" : "How much do they owe you?"}
+      </Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "row",
+          alignSelf: "center",
+          maxHeight: 50,
+          marginBottom: 15,
+        }}
+      >
+        {/* Text inputs for the new transaction */}
+        <TextInput
+          style={{
+            borderColor: "gray",
+            borderWidth: 2,
+            borderRightWidth: 0,
+            width: "70%",
+            borderTopLeftRadius: 18.5,
+            borderBottomLeftRadius: 18.5,
+
+            padding: 12,
+            fontSize: 18,
+          }}
+          onChangeText={onChangeAmount}
+          value={amount}
+          placeholder="Amount"
+          keyboardType="numeric"
+          placeholderTextColor="gray"
+        />
+        <TextInput
+          style={{
+            borderColor: "gray",
+            borderWidth: 2,
+            width: "30%",
+            borderTopRightRadius: 18.5,
+            borderBottomRightRadius: 18.5,
+            padding: 12,
+            fontSize: 18,
+          }}
+          onChangeText={onChangeCurrency}
+          value={currency}
+          placeholder="Currency"
+          placeholderTextColor="gray"
+        />
+      </View>
+      <Text
+        style={{
+          fontSize: 22.5,
+          alignSelf: "flex-start",
+        }}
+      >
+        {isEnabled ? "Why do you owe them?" : "Why do they owe you?"}
+      </Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "row",
+          gap: 5,
+          alignSelf: "center",
+          height: 50,
+          marginBottom: 15,
+          maxHeight: 100,
+        }}
+      >
+        <TextInput
+          style={{
+            height: "auto",
+            borderColor: "gray",
+            borderWidth: 2,
+            width: "100%",
+            borderRadius: 18.5,
+            padding: 12,
+            fontSize: 18,
+          }}
+          onChangeText={onChangeDesc}
+          value={desc}
+          placeholder="Short description"
+          placeholderTextColor="gray"
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "row",
+          gap: 5,
+          alignSelf: "center",
+          maxHeight: 50,
+        }}
+      >
+        {/* Submit button, saves the new transaction to on-device storage */}
+        <Pressable
+          onPress={() => submit()}
+          accessibilityLabel="Submit"
+          style={{
+            backgroundColor: "#1c1c1c",
+            padding: 10,
+            borderRadius: 18.5,
+            paddingHorizontal: 15,
+            width: "100%",
+            flex: 1,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 20, color: "white" }}>Save</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
 }
